7064c2eb517e1af0efd7e357c7000e8efe8f8283
sunwq <sunwq@Ctrip.com>
2016-09-18 20:23:15 +0800

    临时提交

---
+++ Wormday.MomoMath.Biz/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wormday.MomoMath.Biz
+{
+    public class QuestionFactory
+    {
+        
+    }
+
+    public class QuestionSetter
+    {
+        public IList<MathProblem> Create(int quantity)
+        {
+            IList<MathProblem> result = new List<MathProblem>();
+            for (int i = 0; i < quantity; i++)
+            {
+                result.Add(new MathProblem() { Problem="1+1",Except=2});
+            }
+            return result;
+        }
+    }
+
+    public class Addition2FiguresQuestionSetter
+    {
+
+    }
+
+    public class MathProblem
+    {
+        public decimal? Act { get; set; }
+        public decimal Except { get; set; }
+        public string Problem { get; set; }
+    }
+}
---
+++ Wormday.MomoMath.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace Wormday.MomoMath.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Expression<Func<int, int, int, int, int, int>> funcExc3 = (a, b, c, d, e) => (a + b) * (c + d + e);
+            var q = funcExc3.Compile();
+            int r = q(1, 1, 1, 1, 1);
+            Assert.AreEqual(r, 6);
+            
+        }
+    }
+}
