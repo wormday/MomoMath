61b7e5866c0a9dc1def7bddc423d354e6c81d251
wormday <sunwuqiang@gmail.com>
2016-09-20 17:30:21 +0800

    添加了一个统计代码量的临时项目

---
+++ CodeQty/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace CodeQty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.ProcessStartInfo si = new System.Diagnostics.ProcessStartInfo();
+            si.RedirectStandardError = true;
+            si.RedirectStandardInput = true;
+            si.RedirectStandardOutput = true;
+            si.UseShellExecute = false;
+            si.FileName = @"C:\Program Files (x86)\Git\bin\sh.exe";
+            si.Arguments = "--login -i";
+            si.CreateNoWindow = true;	//设置这个属性不创建新窗口。
+
+            System.Diagnostics.Process p = Process.GetCurrentProcess();
+            p.StartInfo = si;
+            p.Start();
+
+            StreamWriter sw = p.StandardInput;
+            sw.WriteLine("git log --stat");
+            sw.WriteLine("exit");
+            StreamReader sr = p.StandardOutput;
+            string str = sr.ReadToEnd();
+            Console.Write(str);
+            Console.ReadLine();
+            p.WaitForExit();
+        }
+    }
+}
